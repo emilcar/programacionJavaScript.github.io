7fd6ecfb8e9a353f5863fec0691249e6054830b2
emilcar <cstafe52@gmail.com>
2026-03-07 20:59:05 -0300

    Add files via upload

--- util.js
+++ util.js
@@ -1,5 +1,90 @@
-function print(mensaje) {
+
+/**
+ * función usada para imprimir en el DOM, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a escribir en el DOM} mensaje 
+ */
+function write(mensaje) {
     document.writeln(mensaje)
+}
+
+/**
+ * función usada para imprimir en el DOM, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a escribir en el DOM} mensaje 
+ */
+function writeln(mensaje) {
+    document.writeln(mensaje + "<br>")
+}
+
+/**
+ * función usada para imprimir en el DOM, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * El nombre es para emular el print de python
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a escribir en el DOM} mensaje 
+ */
+function print(mensaje) {
+    document.writeln(mensaje + "<br>")
+}
+
+/**
+ * función usada para imprimir sin salto de linea en el DOM, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * El nombre es para emular el print de python
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a escribir en el DOM} mensaje 
+ */
+function printx(mensaje) {
+    document.writeln(mensaje)
+}
+
+/**
+ * función usada para imprimir en el DOM, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * El nombre es para emular el print de PSeint
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a escribir en el DOM} mensaje 
+ */
+function imprimir(mensaje) {
+    document.writeln(mensaje + "<br>")
+}
+
+/**
+ * función usada para imprimir sin salto de linea en el DOM, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * El nombre es para emular el print de PSeint
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a escribir en el DOM} mensaje 
+ */
+function imprimirx(mensaje) {
+    document.writeln(mensaje)
+}
+
+/**
+ * función usada para entrada de datos, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * El nombre es para emular un input básico
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a mostrar} mensaje 
+ * @returns valor ingresado
+ */
+function input(mensaje) {
+    return prompt(mensaje)
+}
+
+/**
+ * función usada para entrada de datos, y facilitar el
+ * trabajo educativo a nicel inicial.
+ * El nombre es para emular un input de PSeint
+ * No recomendado para usar en productos destinados a ambientes de producción
+ * @param {mensaje a mostrar} mensaje 
+ * @returns valor ingresado
+ */
+function entrada(mensaje) {
+    return prompt(mensaje)
 }
 const apiWheather = {
     key: '33d58c2db3804b710624d4a5316c6c48',
@@ -8,304 +93,53 @@ const apiWheather = {
     internet: false
 }
 
-function getZonaHoraria() {
-    //El formato de zona horaria lo da el navegador de internet
-    return Intl
-        .DateTimeFormat()
-        .resolvedOptions()
-        .timeZone
-        .replace("/", " ")
-        .replace("/", " ")
-        .replace("_", " ")
-        .replace("America","América")
+
+
+
+/**
+ * Función que devuelve la hora en un string
+ * @returns Hora
+ */
+function getHora() {
+    const fecha = new Date()
+    const hora = fecha.getHours()
+    const minuto = fecha.getMinutes()
+    return hora + " horas y " + minuto + " minutos"
 }
 
 /**
- * Función que convierte grados kelvin a celcius
- * @param {*} kelvin 
- * @returns 
+ * Función que devuelve la fecha actual en un string
+ * @returns Fecha
  */
-function toCelsius(kelvin) {
-    return Math.round(kelvin - 273.15)
-}
+function getFecha() {
+    const fecha = new Date()
+    const diaMes = fecha.getDate()
+    const diaSem = fecha.getDay()
+    const nroMes = fecha.getMonth()
+    const anio = fecha.getFullYear()
 
-/**
- * Devuelve la temperatura ambiente consultada en un API meteorológico
- * @returns temperatura en grados centigrados
- */
-function getTemperatura() {
-    try {
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiWheather.url}?q=${apiWheather.city}&appid=${apiWheather.key}&lang=es`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return toCelsius(json.main.temp)
-        }
-    } catch (error) {
-        return "error"
-    }
-}
+    var nombreDia = ''
+    if (diaSem == 0) nombreDia = 'domingo'
+    if (diaSem == 1) nombreDia = 'lunes'
+    if (diaSem == 2) nombreDia = 'martes'
+    if (diaSem == 3) nombreDia = 'miércoles'
+    if (diaSem == 4) nombreDia = 'jueves'
+    if (diaSem == 5) nombreDia = 'viernes'
+    if (diaSem == 6) nombreDia = 'sábado'
 
-/**
- * Devuelve la sensación térmica ambiente consultada en un API meteorológico
- * @returns temperatura en grados centigrados
- */
-function getSensacionTermica() {
-    try {
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiWheather.url}?q=${apiWheather.city}&appid=${apiWheather.key}&lang=es`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return toCelsius(json.main.feels_like)
-        }
-    } catch (error) {
-        return "error"
-    }
-}
+    var nombreMes = ''
+    if (nroMes == 0) nombreMes = 'enero'
+    if (nroMes == 1) nombreMes = 'febrero'
+    if (nroMes == 2) nombreMes = 'marzo'
+    if (nroMes == 3) nombreMes = 'abril'
+    if (nroMes == 4) nombreMes = 'mayo'
+    if (nroMes == 5) nombreMes = 'junio'
+    if (nroMes == 6) nombreMes = 'julio'
+    if (nroMes == 7) nombreMes = 'agosto'
+    if (nroMes == 8) nombreMes = 'septiembre'
+    if (nroMes == 9) nombreMes = 'octubre'
+    if (nroMes == 10) nombreMes = 'noviembre'
+    if (nroMes == 11) nombreMes = 'diciembre'
 
-/**
- * Devuelve la temperatura mínima consultada en un API meteorológico
- * @returns temperatura en grados centigrados
- */
-function getTemperaturaMin() {
-    try {
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiWheather.url}?q=${apiWheather.city}&appid=${apiWheather.key}&lang=es`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return toCelsius(json.main.temp_min)
-        }
-    } catch (error) {
-        return "error"
-    }
+    return nombreDia + ", " + diaMes + " de " + nombreMes + " del año " + anio
 }
-
-/**
- * Devuelve la temperatura máxima consultada en un API meteorológico
- * @returns temperatura en grados centigrados
- */
-function getTemperaturaMax() {
-    try {
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiWheather.url}?q=${apiWheather.city}&appid=${apiWheather.key}&lang=es`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return toCelsius(json.main.temp_max)
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * Devuelve un string con el clima consultada en un API meteorológico
- * por el momento no se conoce el listado de enums posibles
- * @returns string clima
- */
-function getClima() {
-    try {
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiWheather.url}?q=${apiWheather.city}&appid=${apiWheather.key}&lang=es`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return json.weather[0].description
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * Devuelve un string el nombre de la zona, consultada en un API,
- * https://ipwhois.app/json/ solo se dispones de 10mil request por mes en modo free
- * Se espera solucionar bug detectado en algunas configuraciones usando el API de JS
- * @returns string zona
- */
-function getZona() {
-    try {
-        if (navigator.geolocation) {
-            var success = function (position) {
-                var latitud = position.coords.latitude,
-                    longitud = position.coords.longitude;
-            }
-            navigator.geolocation.getCurrentPosition(success, function (msg) {
-                console.error(msg);
-            });
-        }
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiGeo.url}`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return json.timezone.replace("/", " ").replace("/", " ").replace("_", " ")
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * @deprecated este método esta deprecado por que la API devuelve una región no un continente
- * Devuelve un string el nombre del continente, consultado en un API,
- * https://ipwhois.app/json/ solo se dispones de 10mil request por mes en modo free
- * @returns string continente
- */
-function getContinente() {
-    try {
-        if (navigator.geolocation) {
-            var success = function (position) {
-                var latitud = position.coords.latitude,
-                    longitud = position.coords.longitude
-            }
-            navigator.geolocation.getCurrentPosition(success, function (msg) {
-                console.error(msg);
-            });
-        }
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiGeo.url}`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return json.continent
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * Devuelve la hora en que anochece consultada en un API meteorológico
- * @returns temperatura en grados centigrados
- */
-function getAnochece() {
-    try {
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiWheather.url}?q=${apiWheather.city}&appid=${apiWheather.key}&lang=es`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            var timestamp = json.sys.sunset
-            var fecha = new Date(timestamp * 1000)
-            var anio = fecha.getFullYear()
-            var hora = fecha.getHours()
-            var minuto = fecha.getMinutes()
-            return hora + " horas y " + minuto + " minutos"
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * Devuelve la hora en que anochece consultada en un API meteorológico
- * @returns temperatura en grados centigrados
- */
-function getAmanece() {
-    try {
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiWheather.url}?q=${apiWheather.city}&appid=${apiWheather.key}&lang=es`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            const timestamp = json.sys.sunrise
-            const fecha = new Date(timestamp * 1000)
-            const anio = fecha.getFullYear()
-            const hora = fecha.getHours()
-            const minuto = fecha.getMinutes()
-            return hora + " horas y " + minuto + " minutos"
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * Devuelve un string el nombre de la región, consultado en un API,
- * https://ipwhois.app/json/ solo se dispones de 10mil request por mes en modo free
- * @returns string región
- */
-function getRegion() {
-    try {
-        if (navigator.geolocation) {
-            var success = function (position) {
-                var latitud = position.coords.latitude,
-                    longitud = position.coords.longitude;
-            }
-            navigator.geolocation.getCurrentPosition(success, function (msg) {
-                console.error(msg);
-            });
-        }
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiGeo.url}`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return json.continent
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * Devuelve un string el nombre del país consultada en un API,
- * https://ipwhois.app/json/ solo se dispones de 10mil request por mes en modo free
- * @returns string pais
- */
-function getPais() {
-    try {
-        if (navigator.geolocation) {
-            var success = function (position) {
-                var latitud = position.coords.latitude,
-                    longitud = position.coords.longitude;
-            }
-            navigator.geolocation.getCurrentPosition(success, function (msg) {
-                console.error(msg);
-            });
-        }
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiGeo.url}`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return json.country
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
-/**
- * Devuelve un string el nombre de la provincia consultada en un API,
- * https://ipwhois.app/json/ solo se dispones de 10mil request por mes en modo free
- * basado en la dirección del ISP
- * @returns string provincia
- */
-function getProvincia() {
-    try {
-        if (navigator.geolocation) {
-            var success = function (position) {
-                var latitud = position.coords.latitude,
-                    longitud = position.coords.longitude;
-            }
-            navigator.geolocation.getCurrentPosition(success, function (msg) {
-                console.error(msg);
-            });
-        }
-        const request = new XMLHttpRequest();
-        request.open("GET", `${apiGeo.url}`, false); // `false` makes the request synchronous
-        request.send();
-        if (request.status === 200) {
-            const json = JSON.parse(request.responseText)
-            return json.region
-        }
-    } catch (error) {
-        return "error"
-    }
-}
-
